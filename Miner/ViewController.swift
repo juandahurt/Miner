d33444a2ee2bf132df016e8c37eeecc85784d0cf
Juan David Hurtado <juandahurtadom@gmail.com>
2024-05-29 18:43:19 -0500

    Initial Commit

---
+++ Miner/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Miner
+//
+//  Created by Juan Hurtado on 29/05/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
